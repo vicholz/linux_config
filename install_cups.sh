afd4a773f62f10458b12096ea6978b375a183a98
Victor <vicholz@gmail.com>
2021-04-20 20:42:37 -0700

    Update install_cups.sh

--- install_cups.sh
+++ install_cups.sh
@@ -18,7 +18,7 @@ sudo apt install -y \
 sudo systemctl start cups
 sudo systemctl enable cups
 sudo ufw allow cups
-sed -e -i 's/Order allow,deny/Order allow,deny\n  Allow @LOCAL/g' /tmp/cupsd.conf
+sed -i 's/Order allow,deny/Order allow,deny\n  Allow @LOCAL/g' /tmp/cupsd.conf
 
 # setup avahi
 sudo systemctl start avahi-daemon

6fd2b81ebf16edbc8bc43097c082f6e9b329721e
Victor <vicholz@gmail.com>
2021-09-07 15:54:29 -0700

    fixing username

--- install_scans.sh
+++ install_scans.sh
@@ -18,7 +18,7 @@ addgroup storage || true
 useradd scans
 addgroup scans storage
 echo "scans:scans" | chpasswd
-echo -e "scans\scans\n" | smbpasswd -a media
+echo -e "scans\scans\n" | smbpasswd -a scans
 mkdir -p /storage/scans
 chown scans:scans /storage/scans
 chmod -R 0700 /storage/scans

8ca5a9a48f7ede6e8dc0e968be2a0acd15ea527e
Victor <vicholz@gmail.com>
2021-10-03 17:55:57 -0700

    Add files via upload

---
+++ install_nginx_config_monitor.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# check if root user | re-run if not
+if [ "$(id -u)" != "0" ]; then
+	sudo `dirname $0`/`basename $0`
+	exit
+fi
+
+# update
+apt update
+
+# install packages
+apt -y install \
+    inotify-tools
+
+# create tools dir
+mkdir -p /opt/tools
+
+# install nginx config monitor script
+wget https://raw.githubusercontent.com/vicholz/rpi4_config/master/lib/systemd/system/nginx_config_monitor.service -O /opt/tools/nginx_config_monitor.sh
+chmod a+x /opt/tools/nginx_config_monitor.sh
+
+# add nginx_config_monitor.service
+wget https://raw.githubusercontent.com/vicholz/rpi4_config/master/lib/systemd/system/nginx_config_monitor.service -O /lib/systemd/system/nginx_config_monitor.service
+
+# restart services
+systemctl daemon-reload
+systemctl enable nginx_config_monitor
+systemctl start nginx_config_monitor

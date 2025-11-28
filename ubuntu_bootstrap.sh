927cfa3a370a3d5419ebdccbbe746c9885d7e9ad
Victor <vicholz@gmail.com>
2025-11-27 23:35:39 -0800

    Fix sudo usage for adding Google Chrome repo

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -82,7 +82,7 @@ fi
 
 # Install Google Chrome repo
 wget -qO - https://dl.google.com/linux/linux_signing_key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/google-chrome.gpg > /dev/null
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
+sudo echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list
 apt update
 apt install google-chrome-stable
 

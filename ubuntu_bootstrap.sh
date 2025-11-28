ae2398cb852bcacd8d888fdad7be49f422ceaf44
Victor <vicholz@gmail.com>
2025-11-28 00:02:48 -0800

    Update ubuntu_bootstrap.sh

--- ubuntu_bootstrap.sh
+++ ubuntu_bootstrap.sh
@@ -116,7 +116,7 @@ fi
 END
 )
   echo -n "Adding loader for '\$HOME/.bashrc.d/*.bash' files..."
-  echo "$CFG" > | sudo tee -a /etc/bash.bashrc > /dev/null
+  echo "$CFG" | sudo tee -a /etc/bash.bashrc > /dev/null
 fi
 
 # setup nvidia power management
